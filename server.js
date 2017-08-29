29cdf95364286b520d39c1e91808cdefd73b74d0
Jon Lindell <jhlindell@yahoo.com>
2017-08-29 15:35:25 -0600

    basic pixel updates between multiple browsers working

--- server.js
+++ server.js
@@ -6,11 +6,14 @@ if (process.env.NODE_ENV !== 'production') {
 
 const express = require('express');
 const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 const path = require('path');
 const users = require('./routes/users');
-const port = 8000;
+const apiPort = 8000;
+const socketPort = 7000;
 
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -21,6 +24,20 @@ app.use((req, res, next) => {
   res.sendStatus(404);
 });
 
-app.listen(port, () => {
-  console.log("Now listening on port " + port);
+io.on('connection', (socket) => {
+  // socket.on('room', (room)=> {
+  //   socket.join(room);
+  //   console.log('user joined room: ',room);
+  // });
+  socket.on('pixel', (pixel)=> {
+    socket.emit('pixel', pixel);
+    socket.broadcast.emit('pixel', pixel);
+  })
+});
+
+io.listen(socketPort);
+console.log("Now listening on port " + socketPort);
+
+app.listen(apiPort, () => {
+  console.log("Now listening on port " + apiPort);
 });

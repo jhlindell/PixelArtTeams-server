e72f218f07c623d2699f7e30594800160292b0af
Jon Lindell <jhlindell@yahoo.com>
2017-09-08 09:21:05 -0600

    added header for cors

--- server.js
+++ server.js
@@ -20,6 +20,12 @@ const socketPort = 7000;
 
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 //setup initial pixel grid for socket to track
 var allProjects = [];
 getProjectsFromDatabase();

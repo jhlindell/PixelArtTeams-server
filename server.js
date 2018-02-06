63cd456caffedd4a5dbd45b04e2e5f66418cef3d
Jon Lindell <jhlindell@yahoo.com>
2018-02-06 14:03:54 -0700

    trying to move socket code back into server.js to solve cors issue

--- server.js
+++ server.js
@@ -4,11 +4,39 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const app = require('express')();
 const server = require('http').Server(app);
-const io = require('./socket');
+const io = require('socket.io')();
 const bodyParser = require('body-parser');
 const router = require('./router');
 const cors = require('cors');
 const Port = process.env.PORT || 8000;
+
+const {
+  getProjectsFromDatabase,
+  sendProjectToDatabase,
+  sendFinishedProjectToDatabase,
+  getProjectById,
+  getIndexOfProject,
+  addNewProject,
+  setupNewGrid,
+  deleteUnfinishedProject,
+  galleryArt,
+  changePixel
+} = require('./routes/projects');
+
+const {
+  addUserPermission,
+  checkForUser,
+  getUserProjectsArray,
+  getNameFromToken,
+  getIdFromToken
+} = require('./routes/users');
+
+const winston = require('winston');
+const logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.File)({ filename: 'pixel.log' })
+  ]
+});
 
 // const allowedOrigins = ["https://pixelart-app.herokuapp.com/art", "https://pixelart-app.herokuapp.com/gallery", "https://pixelart-app.herokuapp.com/"];
 
@@ -16,6 +44,117 @@ io.set('origins', '*:*');
 app.use(cors());
 app.use(bodyParser.json());
 router(app);
+
+getProjectsFromDatabase().then((allProjects) => {
+  runProgram(allProjects);
+});
+
+const runProgram = (allProjects) => {
+  io.on('connection', (socket) => {
+    socket.on('joinRoom', (room) => {
+      socket.join(room);
+    });
+
+    socket.on('leaveRoom', (room) => {
+      socket.leave(room);
+    });
+
+    socket.on('grid', (room) =>{
+      try{
+        let index = getIndexOfProject(allProjects, room);
+        socket.emit('gridUpdated', allProjects[index].grid);
+      }
+      catch(err){
+        logger.error(err);
+      }
+    });
+
+    socket.on('pixel', (pixel) => {
+      changePixel(allProjects, pixel);
+      io.in(pixel.project).emit('pixel', pixel);
+    });
+
+    socket.on('initialize', async (token) => {
+      if(token){
+        let projects = await getUserProjectsArray(allProjects, token);
+        socket.emit('sendProjectsToClient', projects);
+      } else {
+        socket.emit('sendProjectsToClient', []);
+      }
+    });
+
+    socket.on('getArtForGallery', () => {
+      galleryArt().then((result) => {
+        socket.emit("sendingGallery", result);
+      });
+    });
+
+    socket.on('addNewProject', async (obj) => {
+      let id = await addNewProject(allProjects, obj);
+      let projects = await getUserProjectsArray(allProjects, obj.token);
+      socket.emit('sendProjectsToClient', projects);
+      socket.broadcast.emit('requestRefresh');
+      socket.emit('changeCurrentProject', id);
+    });
+
+    socket.on('refreshProjects', async (token) => {
+      if(token){
+        let projects = await getUserProjectsArray(allProjects, token);
+        socket.emit('sendProjectsToClient', projects);
+      }
+    })
+
+    socket.on('saveProject', (obj) => {
+      sendProjectToDatabase(allProjects, obj.projectid).then( async() => {
+        let projects = await getUserProjectsArray(allProjects, obj.token);
+        socket.emit('sendProjectsToClient', projects);
+        socket.broadcast.emit('requestRefresh');
+      });
+    });
+
+    socket.on('deleteProject', (obj) => {
+      deleteUnfinishedProject(obj.projectid).then( async() => {
+        let index = getIndexOfProject(allProjects, obj.projectid);
+        allProjects.splice(index, 1);
+        let projects = await getUserProjectsArray(allProjects, obj.token);
+        let firstProjectId = projects[0].project_id;
+        socket.emit('changeCurrentProject', firstProjectId);
+        socket.emit('sendProjectsToClient', projects);
+        socket.broadcast.emit('requestRefresh');
+      });
+    })
+
+    socket.on('sendFinishedProject', async (obj) => {
+      await sendProjectToDatabase(allProjects, obj.projectid);
+      await sendFinishedProjectToDatabase(allProjects, obj.projectid);
+      let projects = await getUserProjectsArray(allProjects, obj.token);
+      let firstProjectId = projects[0].project_id;
+      socket.emit('changeCurrentProject', firstProjectId);
+      socket.emit('sendProjectsToClient', projects);
+      socket.broadcast.emit('requestRefresh');
+    });
+
+    socket.on('addUserToProject', async (obj) => {
+      let userId = await checkForUser(obj.username, obj.email);
+      let result = await addUserPermission(userId, obj.projectid);
+      socket.emit('resultOfAddingPermission', result);
+    });
+
+    socket.on('checkUser', async (obj) => {
+      let userId = await checkForUser(obj.username, obj.email);
+      if(userId){
+        socket.emit("resultOfUserCheck", true);
+      } else {
+        socket.emit("resultOfUserCheck", false);
+      }
+    });
+
+    socket.on('getUserName', (token) => {
+      let username = getNameFromToken(token);
+      socket.emit('returnUserName', username);
+    })
+  });
+}
 
 io.attach(server, {
 	pingInterval: 10000,

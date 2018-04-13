91d1010064f5d9a713bf6c99a8481c06f58fbcde
Jon Lindell <jhlindell@yahoo.com>
2018-04-13 11:11:55 -0600

    stripe donations working

---
+++ routes/payment_api.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const configureStripe = require('stripe');
+const stripe = configureStripe(process.env.STRIPE_SECRET_KEY);
+
+const postStripeCharge = res => (stripeErr, stripeRes) => {
+  if (stripeErr) {
+    res.status(500).send({ error: stripeErr });
+  } else {
+    res.status(200).send({ success: stripeRes });
+  }
+}
+
+router.get('/', (req, res) => {
+  res.send({ message: 'Hello Stripe checkout server!', timestamp: new Date().toISOString() })
+});
+
+router.post('/', (req, res) => {
+  stripe.charges.create(req.body, postStripeCharge(res));
+});
+
+
+module.exports = router;
